131d97a74687b79b39ae783c4d309b513fea248b
floating-cat <monsoon.cl@gmail.com>
2015-04-02 22:14:39 +0800

    Bump dependencies
    * Use OkHttp's `isCacheable(Response)` method instead of ours

--- app/src/main/java/cl/monsoon/s1next/widget/OkHttpStreamFetcher.java
+++ app/src/main/java/cl/monsoon/s1next/widget/OkHttpStreamFetcher.java
@@ -9,8 +9,6 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
-import org.apache.commons.lang3.ArrayUtils;
-
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -18,16 +16,18 @@ import cl.monsoon.s1next.Api;
 import cl.monsoon.s1next.singleton.AvatarUrlCache;
 import cl.monsoon.s1next.singleton.Setting;
 
+import static com.squareup.okhttp.internal.http.StatusLine.HTTP_PERM_REDIRECT;
+import static com.squareup.okhttp.internal.http.StatusLine.HTTP_TEMP_REDIRECT;
 import static java.net.HttpURLConnection.HTTP_BAD_METHOD;
 import static java.net.HttpURLConnection.HTTP_GONE;
 import static java.net.HttpURLConnection.HTTP_MOVED_PERM;
+import static java.net.HttpURLConnection.HTTP_MOVED_TEMP;
 import static java.net.HttpURLConnection.HTTP_MULT_CHOICE;
 import static java.net.HttpURLConnection.HTTP_NOT_AUTHORITATIVE;
 import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
 import static java.net.HttpURLConnection.HTTP_NOT_IMPLEMENTED;
 import static java.net.HttpURLConnection.HTTP_NO_CONTENT;
 import static java.net.HttpURLConnection.HTTP_OK;
-import static java.net.HttpURLConnection.HTTP_PARTIAL;
 import static java.net.HttpURLConnection.HTTP_REQ_TOO_LONG;
 
 
@@ -37,25 +37,6 @@ import static java.net.HttpURLConnection.HTTP_REQ_TOO_LONG;
  * Forked from https://github.com/bumptech/glide/blob/master/integration/okhttp/src/main/java/com/bumptech/glide/integration/okhttp/OkHttpStreamFetcher.java
  */
 final class OkHttpStreamFetcher implements DataFetcher<InputStream> {
-
-    /**
-     * According to RFC, we could cache some responses with following status codes.
-     * <p>
-     * See http://tools.ietf.org/html/rfc7231#section-6.1
-     */
-    private static final int[] CACHEABLE_RESPONSE_STATUS_CODES = new int[]{
-            HTTP_OK,
-            HTTP_NOT_AUTHORITATIVE,
-            HTTP_NO_CONTENT,
-            HTTP_PARTIAL,
-            HTTP_MULT_CHOICE,
-            HTTP_MOVED_PERM,
-            HTTP_NOT_FOUND,
-            HTTP_BAD_METHOD,
-            HTTP_GONE,
-            HTTP_REQ_TOO_LONG,
-            HTTP_NOT_IMPLEMENTED
-    };
 
     private final OkHttpClient mOkHttpClient;
     private final String mUrl;
@@ -93,8 +74,7 @@ final class OkHttpStreamFetcher implements DataFetcher<InputStream> {
             response.body().close();
 
             // if (this this a avatar URL) && (this URL is cacheable)
-            if (key != null && ArrayUtils.contains(
-                    CACHEABLE_RESPONSE_STATUS_CODES, response.code())) {
+            if (key != null && isCacheable(response)) {
                 AvatarUrlCache.put(key);
             }
 
@@ -102,6 +82,48 @@ final class OkHttpStreamFetcher implements DataFetcher<InputStream> {
         }
 
         return response.body().byteStream();
+    }
+
+    /**
+     * Forked form {@link com.squareup.okhttp.internal.http.CacheStrategy#isCacheable(Response, Request)}.
+     */
+    public static boolean isCacheable(Response response) {
+        // Always go to network for uncacheable response codes (RFC 7231 section 6.1),
+        // This implementation doesn't support caching partial content.
+        switch (response.code()) {
+            case HTTP_OK:
+            case HTTP_NOT_AUTHORITATIVE:
+            case HTTP_NO_CONTENT:
+            case HTTP_MULT_CHOICE:
+            case HTTP_MOVED_PERM:
+            case HTTP_NOT_FOUND:
+            case HTTP_BAD_METHOD:
+            case HTTP_GONE:
+            case HTTP_REQ_TOO_LONG:
+            case HTTP_NOT_IMPLEMENTED:
+            case HTTP_PERM_REDIRECT:
+                // These codes can be cached unless headers forbid it.
+                break;
+
+            case HTTP_MOVED_TEMP:
+            case HTTP_TEMP_REDIRECT:
+                // These codes can only be cached with the right response headers.
+                // http://tools.ietf.org/html/rfc7234#section-3
+                // s-maxage is not checked because OkHttp is a private cache that should ignore s-maxage.
+                if (response.header("Expires") != null
+                        || response.cacheControl().maxAgeSeconds() != -1
+                        || response.cacheControl().isPublic()
+                        || response.cacheControl().isPrivate()) {
+                    break;
+                }
+                // Fall-through.
+
+            default:
+                // All other codes cannot be cached.
+                return false;
+        }
+
+        return true;
     }
 
     @Override
