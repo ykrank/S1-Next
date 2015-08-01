e6e40e62d919a8a0277bc85e657d1b4554c733bc
floating-cat <monsoon.cl@gmail.com>
2015-08-01 17:51:13 +0800

    Add ripple effect to drawer header background if API >= 21

---
+++ app/src/main/java/cl/monsoon/s1next/binding/ImageViewBindingAdapter.java
@@ -0,0 +1,28 @@
+package cl.monsoon.s1next.binding;
+
+import android.content.res.ColorStateList;
+import android.databinding.BindingAdapter;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.RippleDrawable;
+import android.os.Build;
+import android.support.annotation.ColorInt;
+import android.widget.ImageView;
+
+import cl.monsoon.s1next.R;
+
+public final class ImageViewBindingAdapter {
+
+    @BindingAdapter("imageDrawable")
+    public static void setImageDrawable(ImageView imageView, Drawable drawable) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            @ColorInt int rippleColor = imageView.getContext().getResources().getColor(
+                    R.color.ripple_material_dark);
+            // add ripple effect
+            RippleDrawable rippleDrawable = new RippleDrawable(ColorStateList.valueOf(rippleColor),
+                    drawable, null);
+            imageView.setImageDrawable(rippleDrawable);
+        } else {
+            imageView.setImageDrawable(drawable);
+        }
+    }
+}
