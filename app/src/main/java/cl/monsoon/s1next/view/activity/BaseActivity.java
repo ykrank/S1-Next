407c886bbe04b66aa81db8282c43c5afa7a8764a
floating-cat <monsoon.cl@gmail.com>
2015-08-06 10:47:25 +0800

    Fix: unable to change font size
    Due to 524737d9380bfba39c64b6ff18b8186b16ddba93

--- app/src/main/java/cl/monsoon/s1next/view/activity/BaseActivity.java
+++ app/src/main/java/cl/monsoon/s1next/view/activity/BaseActivity.java
@@ -67,7 +67,7 @@ public abstract class BaseActivity extends RxAppCompatActivity {
 
         mEventBus.get().compose(bindUntilEvent(ActivityEvent.DESTROY)).subscribe(o -> {
             // recreate this Activity when theme or font size changes
-            if (o instanceof ThemeChangeEvent ) {
+            if (o instanceof ThemeChangeEvent || o instanceof FontSizeChangeEvent) {
                 getWindow().setWindowAnimations(R.style.Animation_Recreate);
                 recreate();
             }
