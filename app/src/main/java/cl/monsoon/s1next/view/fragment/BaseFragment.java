21d309352bf70f9f9eee3c248a983e229328051d
floating-cat <monsoon.cl@gmail.com>
2015-08-10 16:12:22 +0800

    Use `Observable#doOnNext(Action1)` to intercept user's login status

--- app/src/main/java/cl/monsoon/s1next/view/fragment/BaseFragment.java
+++ app/src/main/java/cl/monsoon/s1next/view/fragment/BaseFragment.java
@@ -205,7 +205,7 @@ public abstract class BaseFragment<D> extends RxFragment {
         getSourceObservable().compose(bindUntilEvent(FragmentEvent.DESTROY))
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .map(mUserValidator::validateIntercept)
+                .doOnNext(mUserValidator::validateIntercept)
                 .finallyDo(this::finallyDo)
                 .subscribe(this::onNext, this::onError);
     }
