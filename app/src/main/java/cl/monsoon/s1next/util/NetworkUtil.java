faa7889abfbb53c31dbab625e755e139dfbda83b
floating-cat <monsoon.cl@gmail.com>
2015-08-18 16:51:01 +0800

    Check multiple connected networks if API >= 21

--- app/src/main/java/cl/monsoon/s1next/util/NetworkUtil.java
+++ app/src/main/java/cl/monsoon/s1next/util/NetworkUtil.java
@@ -2,7 +2,10 @@ package cl.monsoon.s1next.util;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.Network;
 import android.net.NetworkInfo;
+import android.os.Build;
+import android.support.annotation.Nullable;
 
 public final class NetworkUtil {
 
@@ -14,10 +17,25 @@ public final class NetworkUtil {
      *
      * @return {@code true} if the Wi-FI network connectivity exists, {@code false} otherwise.
      */
+    @SuppressWarnings("deprecation")
     public static boolean isWifiConnected(Context context) {
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(
                 Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            for (Network network : connectivityManager.getAllNetworks()) {
+                if (isNetworkInfoConnected(connectivityManager.getNetworkInfo(network))) {
+                    return true;
+                }
+            }
+        } else {
+            return isNetworkInfoConnected(connectivityManager.getNetworkInfo(
+                    ConnectivityManager.TYPE_WIFI));
+        }
+
+        return false;
+    }
+
+    private static boolean isNetworkInfoConnected(@Nullable NetworkInfo networkInfo) {
         return networkInfo != null && networkInfo.isConnected();
     }
 }
