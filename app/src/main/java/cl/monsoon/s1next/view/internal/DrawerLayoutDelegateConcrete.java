7d2c40ec5d47b860954042ab687bf21077f11b23
floating-cat <monsoon.cl@gmail.com>
2015-11-06 16:48:59 +0800

    Navigation drawer header should have a 16:9 background image

--- app/src/main/java/cl/monsoon/s1next/view/internal/DrawerLayoutDelegateConcrete.java
+++ app/src/main/java/cl/monsoon/s1next/view/internal/DrawerLayoutDelegateConcrete.java
@@ -1,7 +1,6 @@
 package cl.monsoon.s1next.view.internal;
 
 import android.content.Intent;
-import android.content.res.Resources;
 import android.databinding.DataBindingUtil;
 import android.graphics.Color;
 import android.os.Build;
@@ -69,16 +68,10 @@ public final class DrawerLayoutDelegateConcrete extends DrawerLayoutDelegate
             drawerLayout.setOnApplyWindowInsetsListener((v, insets) -> {
                 int insetsTop = insets.getSystemWindowInsetTop();
 
-                Resources resources = v.getContext().getResources();
-                binding.drawerHeaderBackground.getLayoutParams().height = insetsTop
-                        + resources.getDimensionPixelSize(R.dimen.drawer_top_height);
-                binding.drawerHeaderBackgroundScrim.getLayoutParams().height =
-                        binding.drawerHeaderBackground.getLayoutParams().height;
-
                 ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams)
                         binding.drawerUserAvatar.getLayoutParams();
-                marginLayoutParams.topMargin = insetsTop + resources.getDimensionPixelSize(
-                        R.dimen.drawer_avatar_margin_top);
+                marginLayoutParams.topMargin = insetsTop + v.getContext().getResources()
+                        .getDimensionPixelSize(R.dimen.drawer_avatar_margin_top);
 
                 // see https://github.com/android/platform_frameworks_support/blob/master/v4/api21/android/support/v4/widget/DrawerLayoutCompatApi21.java#L86
                 // add DrawerLayout's default View.OnApplyWindowInsetsListener implementation
