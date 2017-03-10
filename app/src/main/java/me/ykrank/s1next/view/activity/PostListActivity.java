f11f2637f5533dbeb0cdb4a6a546720632d15235
ykrank <yuanke@sencent.com>
2017-03-10 18:47:31 +0800

    Revert "PostList use Immersive Mode"
    This reverts commit d5fd036d164ea1ada7a8ac0a779fef1af1fda041.

--- app/src/main/java/me/ykrank/s1next/view/activity/PostListActivity.java
+++ app/src/main/java/me/ykrank/s1next/view/activity/PostListActivity.java
@@ -153,25 +153,4 @@ public final class PostListActivity extends BaseActivity
         }
     }
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-//            if (hasFocus) {
-//                getWindow().addFlags(FLAG_TRANSLUCENT_NAVIGATION);
-//                getWindow().getDecorView().setSystemUiVisibility(
-//                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                                | View.SYSTEM_UI_FLAG_FULLSCREEN
-//                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-//            }
-//        }
-    }
-
-    @Override
-    public boolean isTranslucent() {
-        return true;
-    }
 }
