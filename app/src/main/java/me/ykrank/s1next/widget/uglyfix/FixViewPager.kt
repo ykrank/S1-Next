7b3383d722b53ef79f7df4ba85214deece1f1e82
ykrank <yuanke@sencent.com>
2017-07-20 19:11:10 +0800

    Use fixedViewPager ignore  some system exception

---
+++ app/src/main/java/me/ykrank/s1next/widget/uglyfix/FixViewPager.kt
@@ -0,0 +1,25 @@
+package me.ykrank.s1next.widget.uglyfix
+
+import android.content.Context
+import android.support.v4.view.ViewPager
+import android.util.AttributeSet
+import android.view.MotionEvent
+import java.lang.Exception
+
+/**
+ * Viewpager sometimes throw java.lang.IllegalArgumentException pointerIndex out of range in 6.0 (api 23)
+ */
+class FixViewPager : ViewPager {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
+        try {
+            return super.onInterceptTouchEvent(ev)
+        } catch (e: Exception) {
+            return false
+        }
+    }
+}
