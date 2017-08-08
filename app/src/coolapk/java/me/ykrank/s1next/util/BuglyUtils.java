f8e568ce2e122272bf4568f4795312f89eab671b
ykrank <ykrank@gmail.com>
2017-08-09 01:28:52 +0800

    Disable hotfix

--- app/src/coolapk/java/me/ykrank/s1next/util/BuglyUtils.java
+++ app/src/coolapk/java/me/ykrank/s1next/util/BuglyUtils.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 
 import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
 import com.tencent.bugly.crashreport.CrashReport;
 
 import me.ykrank.s1next.BuildConfig;
@@ -21,6 +22,8 @@ public class BuglyUtils {
         RxJavaUtil.workInRxIoThread(() -> {
             CrashReport.UserStrategy userStrategy = new CrashReport.UserStrategy(appContext);
             userStrategy.setAppVersion(BuildConfig.VERSION_NAME + "-" + BuildConfig.VERSION_CODE);
+
+            Beta.enableHotfix = false;
             Bugly.init(appContext, BUGLY_APP_ID, BuildConfig.DEBUG, userStrategy);
         });
     }
