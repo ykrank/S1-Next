b14475c57cee5bc27b0e02697db910cfebfefcf6
ykrank <ykrank@gmail.com>
2017-09-17 19:52:01 +0800

    Fix unicode parse error

--- app/src/main/java/me/ykrank/s1next/util/StringUtil.java
+++ app/src/main/java/me/ykrank/s1next/util/StringUtil.java
@@ -72,21 +72,27 @@ public final class StringUtil {
         char[] chars = s.toCharArray();
         for (int i = 0; i < chars.length; i++) {
             char c = chars[i];
-            if (c == '\\' && chars[i + 1] == 'u') {
-                char cc = 0;
-                for (int j = 0; j < 4; j++) {
-                    char ch = Character.toLowerCase(chars[i + 2 + j]);
-                    if ('0' <= ch && ch <= '9' || 'a' <= ch && ch <= 'f') {
-                        cc |= (Character.digit(ch, 16) << (3 - j) * 4);
-                    } else {
-                        cc = 0;
-                        break;
-                    }
-                }
-                if (cc > 0) {
-                    i += 5;
-                    sb.append(cc);
+            if (c == '\\') {
+                if (chars[i + 1] == '\\') {
+                    i++;
+                    sb.append("\\\\");
                     continue;
+                } else if (chars[i + 1] == 'u') {
+                    char cc = 0;
+                    for (int j = 0; j < 4; j++) {
+                        char ch = Character.toLowerCase(chars[i + 2 + j]);
+                        if ('0' <= ch && ch <= '9' || 'a' <= ch && ch <= 'f') {
+                            cc |= (Character.digit(ch, 16) << (3 - j) * 4);
+                        } else {
+                            cc = 0;
+                            break;
+                        }
+                    }
+                    if (cc > 0) {
+                        i += 5;
+                        sb.append(cc);
+                        continue;
+                    }
                 }
             }
             sb.append(c);
