75061af10a51ad5c22a3cc1c2933e5192a5d65d0
ykrank <ykrank@gmail.com>
2017-10-13 15:18:25 +0800

    Disable hardware Bitmap after SDK O,  because it could not draw on software canvas

--- app/src/main/java/me/ykrank/s1next/widget/BezelImageView.java
+++ app/src/main/java/me/ykrank/s1next/widget/BezelImageView.java
@@ -20,14 +20,16 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.support.annotation.NonNull;
+import android.os.Build;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
@@ -43,18 +45,23 @@ import me.ykrank.s1next.R;
  */
 public final class BezelImageView extends AppCompatImageView {
 
-    private Drawable mBorderDrawable;
-    private Drawable mMaskDrawable;
-
+    private Paint mBlackPaint;
     private Paint mMaskedPaint;
 
     private Rect mBounds;
     private RectF mBoundsF;
 
+    private Drawable mBorderDrawable;
+    private Drawable mMaskDrawable;
+
+    private ColorMatrixColorFilter mDesaturateColorFilter;
+    private boolean mDesaturateOnPress = false;
+
+    private boolean mCacheValid = false;
     private Bitmap mCacheBitmap;
-    private boolean mCacheValid;
     private int mCachedWidth;
     private int mCachedHeight;
+    private Context mContext;
 
     public BezelImageView(Context context) {
         this(context, null);
@@ -64,40 +71,55 @@ public final class BezelImageView extends AppCompatImageView {
         this(context, attrs, 0);
     }
 
-    public BezelImageView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    public BezelImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mContext = context;
+        // Attribute initialization
+        final TypedArray a = context.obtainStyledAttributes(attrs,
+                R.styleable.BezelImageView, defStyle, 0);
 
-        init(context, attrs, defStyleAttr, 0);
-    }
-
-    private void init(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        // attribute initialization
-        final TypedArray typedArray = context.obtainStyledAttributes(attrs,
-                R.styleable.BezelImageView, defStyleAttr, defStyleRes);
-
-        mMaskDrawable = typedArray.getDrawable(R.styleable.BezelImageView_maskDrawable);
+        mMaskDrawable = a.getDrawable(R.styleable.BezelImageView_maskDrawable);
         if (mMaskDrawable != null) {
             mMaskDrawable.setCallback(this);
         }
 
-        mBorderDrawable = typedArray.getDrawable(R.styleable.BezelImageView_borderDrawable);
+        mBorderDrawable = a
+                .getDrawable(R.styleable.BezelImageView_borderDrawable);
         if (mBorderDrawable != null) {
             mBorderDrawable.setCallback(this);
         }
 
-        typedArray.recycle();
+        mDesaturateOnPress = a.getBoolean(
+                R.styleable.BezelImageView_desaturateOnPress,
+                mDesaturateOnPress);
+
+        a.recycle();
+        otherInit();
+    }
+
+    private void otherInit() {
+        // Other initialization
+        mBlackPaint = new Paint();
+        mBlackPaint.setColor(0xff000000);
 
         mMaskedPaint = new Paint();
-        mMaskedPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        mMaskedPaint
+                .setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
 
-        // always want a cache allocated
+        // Always want a cache allocated.
         mCacheBitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
+
+        if (mDesaturateOnPress) {
+            // Create a desaturate color filter for pressed state.
+            ColorMatrix cm = new ColorMatrix();
+            cm.setSaturation(0);
+            mDesaturateColorFilter = new ColorMatrixColorFilter(cm);
+        }
     }
 
     @Override
     protected boolean setFrame(int l, int t, int r, int b) {
         final boolean changed = super.setFrame(l, t, r, b);
-
         mBounds = new Rect(0, 0, r - l, b - t);
         mBoundsF = new RectF(mBounds);
 
@@ -116,40 +138,66 @@ public final class BezelImageView extends AppCompatImageView {
     }
 
     @Override
-    protected void onDraw(@NonNull Canvas canvas) {
+    protected void onDraw(Canvas canvas) {
         if (mBounds == null) {
             return;
         }
 
         int width = mBounds.width();
         int height = mBounds.height();
+
         if (width == 0 || height == 0) {
             return;
         }
 
-        if (!mCacheValid || mCachedWidth != width || mCachedHeight != height) {
-            // need to redraw the cache
-            if (mCachedWidth == width && mCachedHeight == height) {
-                // have a correct-sized bitmap cache already allocated
-                // just erase it
-                mCacheBitmap.eraseColor(Color.TRANSPARENT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //Disable hardware Bitmap because it could not draw on software canvas
+            Drawable mDrawable = getDrawable();
+            if (mDrawable instanceof BitmapDrawable) {
+                Bitmap bitmap = ((BitmapDrawable) mDrawable).getBitmap();
+                if (bitmap != null && bitmap.getConfig() == Bitmap.Config.HARDWARE) {
+                    setImageBitmap(bitmap.copy(Bitmap.Config.ARGB_8888, false));
+                }
+            }
+        }
+
+        if (!mCacheValid || width != mCachedWidth || height != mCachedHeight) {
+            // Need to redraw the cache
+            if (width == mCachedWidth && height == mCachedHeight) {
+                // Have a correct-sized bitmap cache already allocated. Just
+                // erase it.
+                mCacheBitmap.eraseColor(0);
             } else {
-                // allocate a new bitmap with the correct dimensions
+                // Allocate a new bitmap with the correct dimensions.
                 mCacheBitmap.recycle();
-                //noinspection AndroidLintDrawAllocation
-                mCacheBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+                // noinspection AndroidLintDrawAllocation
+                mCacheBitmap = Bitmap.createBitmap(width, height,
+                        Bitmap.Config.ARGB_8888);
                 mCachedWidth = width;
                 mCachedHeight = height;
             }
 
             Canvas cacheCanvas = new Canvas(mCacheBitmap);
             if (mMaskDrawable != null) {
-                int saveCount = cacheCanvas.save();
+                int sc = cacheCanvas.save();
                 mMaskDrawable.draw(cacheCanvas);
+                mMaskedPaint.setColorFilter((mDesaturateOnPress && isPressed()) ? mDesaturateColorFilter
+                        : null);
                 cacheCanvas.saveLayer(mBoundsF, mMaskedPaint,
-                        Canvas.HAS_ALPHA_LAYER_SAVE_FLAG | Canvas.FULL_COLOR_LAYER_SAVE_FLAG);
+                        Canvas.HAS_ALPHA_LAYER_SAVE_FLAG
+                                | Canvas.FULL_COLOR_LAYER_SAVE_FLAG);
                 super.onDraw(cacheCanvas);
-                cacheCanvas.restoreToCount(saveCount);
+                cacheCanvas.restoreToCount(sc);
+            } else if (mDesaturateOnPress && isPressed()) {
+                int sc = cacheCanvas.save();
+                cacheCanvas.drawRect(0, 0, mCachedWidth, mCachedHeight,
+                        mBlackPaint);
+                mMaskedPaint.setColorFilter(mDesaturateColorFilter);
+                cacheCanvas.saveLayer(mBoundsF, mMaskedPaint,
+                        Canvas.HAS_ALPHA_LAYER_SAVE_FLAG
+                                | Canvas.FULL_COLOR_LAYER_SAVE_FLAG);
+                super.onDraw(cacheCanvas);
+                cacheCanvas.restoreToCount(sc);
             } else {
                 super.onDraw(cacheCanvas);
             }
@@ -159,37 +207,36 @@ public final class BezelImageView extends AppCompatImageView {
             }
         }
 
-        // draw from cache
+        // Draw from cache
         canvas.drawBitmap(mCacheBitmap, mBounds.left, mBounds.top, null);
     }
 
     @Override
     protected void drawableStateChanged() {
         super.drawableStateChanged();
-
         if (mBorderDrawable != null && mBorderDrawable.isStateful()) {
             mBorderDrawable.setState(getDrawableState());
         }
         if (mMaskDrawable != null && mMaskDrawable.isStateful()) {
             mMaskDrawable.setState(getDrawableState());
         }
-
         if (isDuplicateParentStateEnabled()) {
             ViewCompat.postInvalidateOnAnimation(this);
         }
     }
 
     @Override
-    protected boolean verifyDrawable(Drawable dr) {
-        return dr == mBorderDrawable || dr == mMaskDrawable || super.verifyDrawable(dr);
+    public void invalidateDrawable(Drawable who) {
+        if (who == mBorderDrawable || who == mMaskDrawable) {
+            invalidate();
+        } else {
+            super.invalidateDrawable(who);
+        }
     }
 
     @Override
-    public void invalidateDrawable(@NonNull Drawable dr) {
-        if (dr == mBorderDrawable || dr == mMaskDrawable) {
-            invalidate();
-        } else {
-            super.invalidateDrawable(dr);
-        }
+    protected boolean verifyDrawable(Drawable who) {
+        return who == mBorderDrawable || who == mMaskDrawable
+                || super.verifyDrawable(who);
     }
 }
