5ee38d5746af262d173c5050b96583127341d205
ykrank <ykrank@gmail.com>
2017-11-13 01:40:04 +0800

    Move base progress dialog to lib

--- app/src/main/java/me/ykrank/s1next/view/dialog/ProgressDialogFragment.kt
+++ app/src/main/java/me/ykrank/s1next/view/dialog/ProgressDialogFragment.kt
@@ -1,25 +1,19 @@
 package me.ykrank.s1next.view.dialog
 
 import android.app.Activity
-import android.app.Dialog
 import android.app.ProgressDialog
 import android.os.Bundle
 import android.support.annotation.CallSuper
 import android.support.design.widget.Snackbar
 import android.support.v4.app.DialogFragment
 import android.widget.Toast
-import com.github.ykrank.androidautodispose.AndroidRxDispose
-import com.github.ykrank.androidlifecycle.event.FragmentEvent
-import com.github.ykrank.androidtools.ui.internal.CoordinatorLayoutAnchorDelegate
+import com.github.ykrank.androidtools.ui.dialog.LibProgressDialogFragment
 import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import me.ykrank.s1next.App
 import me.ykrank.s1next.data.User
 import me.ykrank.s1next.data.api.ApiFlatTransformer
 import me.ykrank.s1next.data.api.S1Service
 import me.ykrank.s1next.data.api.UserValidator
-import me.ykrank.s1next.util.ErrorUtil
 import me.ykrank.s1next.view.activity.BaseActivity
 import me.ykrank.s1next.view.fragment.BaseRecyclerViewFragment
 import javax.inject.Inject
@@ -34,13 +28,9 @@ import javax.inject.Inject
 
  * @param <D> The data we want to request.
 </D> */
-abstract class ProgressDialogFragment<D> : BaseDialogFragment() {
-
-    @Inject
+abstract class ProgressDialogFragment<D> : LibProgressDialogFragment<D>() {
     internal lateinit var mS1Service: S1Service
-    @Inject
     internal lateinit var mUserValidator: UserValidator
-    @Inject
     internal lateinit var mUser: User
 
     private var dialogNotCancelableOnTouchOutside: Boolean = false
@@ -52,48 +42,6 @@ abstract class ProgressDialogFragment<D> : BaseDialogFragment() {
         mS1Service = appComponent.s1Service
         mUser = appComponent.user
         mUserValidator = appComponent.userValidator
-
-        if (arguments != null) {
-            dialogNotCancelableOnTouchOutside = arguments.getBoolean(ARG_DIALOG_NOT_CANCELABLE_ON_TOUCH_OUTSIDE, false)
-        }
-
-        // retain this Fragment
-        retainInstance = true
-
-        request()
-    }
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val progressDialog = ProgressDialog(context)
-        progressDialog.setMessage(getProgressMessage())
-        //press back will remove this fragment, so set cancelable no effect
-        progressDialog.setCanceledOnTouchOutside(!dialogNotCancelableOnTouchOutside)
-
-        return progressDialog
-    }
-
-    @CallSuper
-    override fun onDestroyView() {
-        // see https://code.google.com/p/android/issues/detail?id=17423
-        val dialog = dialog
-        if (dialog != null) {
-            getDialog().setOnDismissListener(null)
-        }
-
-        super.onDestroyView()
-    }
-
-    /**
-     * @see BaseRecyclerViewFragment.load
-     */
-    private fun request() {
-        getSourceObservable()
-                .compose(ApiFlatTransformer.apiErrorTransformer<D>())
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .doAfterTerminate { this.finallyDo() }
-                .to(AndroidRxDispose.withSingle<D>(this, FragmentEvent.DESTROY))
-                .subscribe({ this.onNext(it) }, { this.onError(it) })
     }
 
     /**
@@ -101,37 +49,16 @@ abstract class ProgressDialogFragment<D> : BaseDialogFragment() {
      */
     protected abstract fun getSourceObservable(): Single<D>
 
+    override fun getLibSourceObservable(): Single<D> {
+        return getSourceObservable()
+                .compose(ApiFlatTransformer.apiErrorTransformer<D>())
+    }
+
     /**
      * @see ApiFlatTransformer.flatMappedWithAuthenticityToken
      */
     protected fun flatMappedWithAuthenticityToken(func: (String) -> Single<D>): Single<D> {
         return ApiFlatTransformer.flatMappedWithAuthenticityToken(mS1Service, mUserValidator, mUser, func)
-    }
-
-    /**
-     * @see BaseRecyclerViewFragment.onNext
-     */
-    protected abstract fun onNext(data: D)
-
-    /**
-     * @see BaseRecyclerViewFragment.onError
-     */
-    internal open fun onError(throwable: Throwable) {
-        showShortText(ErrorUtil.parse(context, throwable))
-    }
-
-    /**
-     * @see BaseRecyclerViewFragment.finallyDo
-     */
-    protected fun finallyDo() {
-        dismissAllowingStateLoss()
-    }
-
-    /**
-     * @see me.ykrank.s1next.view.activity.BaseActivity.showShortText
-     */
-    protected fun showShortText(text: CharSequence?) {
-        (activity as CoordinatorLayoutAnchorDelegate).showShortText(text)
     }
 
     /**
@@ -158,8 +85,6 @@ abstract class ProgressDialogFragment<D> : BaseDialogFragment() {
 
         activity.finish()
     }
-
-    protected abstract fun getProgressMessage(): CharSequence?
 
     companion object {
 
