c7a976b9bbe1339532c2a50520c9cb60e9dd199e
ykrank <ykrank@gmail.com>
2018-05-14 11:42:29 +0800

    Fix textview drawableTop use sector error before api 21

--- app/src/main/java/me/ykrank/s1next/binding/ViewBindingAdapter.java
+++ app/src/main/java/me/ykrank/s1next/binding/ViewBindingAdapter.java
@@ -1,21 +1,11 @@
 package me.ykrank.s1next.binding;
 
 import android.content.Context;
-import android.content.res.ColorStateList;
 import android.databinding.BindingAdapter;
 import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.RippleDrawable;
 import android.os.Build;
-import android.support.annotation.ColorInt;
 import android.support.annotation.Nullable;
-import android.support.v4.graphics.drawable.DrawableCompat;
-import android.support.v4.view.TintableBackgroundView;
-import android.support.v4.view.ViewCompat;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,7 +18,6 @@ import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.Target;
-import com.github.ykrank.androidtools.util.ColorDrawableUtils;
 import com.github.ykrank.androidtools.util.L;
 import com.github.ykrank.androidtools.util.RxJavaUtil;
 import com.github.ykrank.androidtools.widget.glide.transformations.BlurTransformation;
@@ -45,114 +34,6 @@ import me.ykrank.s1next.data.pref.DownloadPreferencesManager;
  */
 public final class ViewBindingAdapter {
     private ViewBindingAdapter() {
-    }
-
-    private static final int UNABLE = Color.LTGRAY;
-    //Default use darker color
-    private static final int RIPPLE_DEFAULT = Integer.MIN_VALUE;
-    private static final ColorDrawable UNABLE_DRAWABLE = new ColorDrawable(UNABLE);
-
-    @BindingAdapter("backTintColor")
-    public static void setBackgroundTint(View view, @ColorInt int oldTintColor, @ColorInt int tintColor) {
-        setBackgroundTint(view, oldTintColor, PorterDuff.Mode.SRC_IN, RIPPLE_DEFAULT, tintColor, PorterDuff.Mode.SRC_IN, RIPPLE_DEFAULT);
-    }
-
-    @BindingAdapter({"backTintColor", "tintMode"})
-    public static void setBackgroundTint(View view, @ColorInt int oldTintColor, @Nullable PorterDuff.Mode oldTintMode,
-                                         @ColorInt int tintColor, @Nullable PorterDuff.Mode tintMode) {
-        setBackgroundTint(view, oldTintColor, oldTintMode, RIPPLE_DEFAULT, tintColor, tintMode, RIPPLE_DEFAULT);
-    }
-
-    @BindingAdapter({"backTintColor", "ripple"})
-    public static void setBackgroundTint(View view, @ColorInt int oldTintColor, int oldRipple,
-                                         @ColorInt int tintColor, int ripple) {
-        setBackgroundTint(view, oldTintColor, PorterDuff.Mode.SRC_IN, oldRipple, tintColor, PorterDuff.Mode.SRC_IN, ripple);
-    }
-
-    @BindingAdapter({"backTintColor", "tintMode", "ripple"})
-    public static void setBackgroundTint(View view, @ColorInt int oldTintColor, @Nullable PorterDuff.Mode oldTintMode, int oldRipple,
-                                         @ColorInt int tintColor, @Nullable PorterDuff.Mode tintMode, int ripple) {
-        if (oldTintColor == tintColor && oldTintMode == tintMode && oldRipple == ripple) {
-            return;
-        }
-        Drawable originalDrawable = view.getBackground();
-        if (originalDrawable == null) {
-            return;
-        }
-
-        if (ripple == RIPPLE_DEFAULT) {
-            ripple = ColorDrawableUtils.getDarkerColor(tintColor);
-        }
-        ColorStateList colorStateList = ColorDrawableUtils.createColorStateList(tintColor, UNABLE);
-
-        //Appcompat view
-        if (view instanceof TintableBackgroundView) {
-            if (tintMode != null) {
-                ((TintableBackgroundView) view).setSupportBackgroundTintMode(tintMode);
-            }
-            ((TintableBackgroundView) view).setSupportBackgroundTintList(colorStateList);
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                RippleDrawable tintDrawable;
-                originalDrawable = view.getBackground();
-                if (originalDrawable instanceof RippleDrawable) {
-                    tintDrawable = ColorDrawableUtils.safeMutableDrawable((RippleDrawable) originalDrawable);
-                    tintDrawable.setColor(ColorStateList.valueOf(ripple));
-                } else {
-                    tintDrawable = ColorDrawableUtils.getRippleDrawable(originalDrawable, ripple);
-                }
-
-                ViewCompat.setBackground(view, tintDrawable);
-            }
-            return;
-        }
-
-        Drawable tintDrawable;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            tintDrawable = ColorDrawableUtils.safeMutableDrawable(originalDrawable);
-            if (tintMode != null) {
-                tintDrawable.setTintMode(tintMode);
-            }
-            tintDrawable.setTintList(colorStateList);
-            if (tintDrawable instanceof RippleDrawable) {
-                ((RippleDrawable) tintDrawable).setColor(ColorStateList.valueOf(ripple));
-            } else {
-                tintDrawable = ColorDrawableUtils.getRippleDrawable(tintDrawable, ripple);
-            }
-            ViewCompat.setBackground(view, tintDrawable);
-        } else {
-            tintDrawable = DrawableCompat.wrap(originalDrawable.mutate());
-            if (tintMode != null) {
-                DrawableCompat.setTintMode(tintDrawable, tintMode);
-            }
-            DrawableCompat.setTintList(tintDrawable, colorStateList);
-            ViewCompat.setBackground(view, tintDrawable);
-            if (tintDrawable == originalDrawable) {
-                view.invalidate();
-            }
-        }
-    }
-
-    @BindingAdapter({"ripple"})
-    public static void setRipple(View view, int oRipple, int ripple) {
-        if (oRipple == ripple) {
-            return;
-        }
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            return;
-        }
-        Drawable tintDrawable = null;
-        Drawable originalDrawable = view.getBackground();
-
-        if (originalDrawable != null) {
-            tintDrawable = ColorDrawableUtils.safeMutableDrawable(originalDrawable);
-        }
-        if (tintDrawable instanceof RippleDrawable) {
-            ((RippleDrawable) tintDrawable).setColor(ColorStateList.valueOf(ripple));
-        } else {
-            tintDrawable = ColorDrawableUtils.getRippleDrawable(tintDrawable, ripple);
-        }
-        ViewCompat.setBackground(view, tintDrawable);
     }
 
     @BindingAdapter("marginEnd")
---
+++ app/src/main/java/me/ykrank/s1next/view/activity/SearchActivity.kt
@@ -0,0 +1,301 @@
+package me.ykrank.s1next.view.activity
+
+import android.app.Activity
+import android.app.SearchManager
+import android.app.SharedElementCallback
+import android.content.Context
+import android.content.Intent
+import android.databinding.DataBindingUtil
+import android.graphics.Point
+import android.graphics.Typeface
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.StateListDrawable
+import android.os.Build
+import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.support.transition.TransitionManager
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.ActivityOptionsCompat
+import android.support.v4.view.ViewCompat
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.text.InputType
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.TextUtils
+import android.text.style.StyleSpan
+import android.transition.Transition
+import android.transition.TransitionInflater
+import android.transition.TransitionSet
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.ImageButton
+import android.widget.SearchView
+import android.widget.TextView
+import com.github.ykrank.androidautodispose.AndroidRxDispose
+import com.github.ykrank.androidlifecycle.event.ActivityEvent
+import com.github.ykrank.androidtools.util.*
+import me.ykrank.s1next.App
+import me.ykrank.s1next.R
+import me.ykrank.s1next.data.api.ApiFlatTransformer
+import me.ykrank.s1next.data.api.S1Service
+import me.ykrank.s1next.data.api.UserValidator
+import me.ykrank.s1next.data.api.model.search.ForumSearchWrapper
+import me.ykrank.s1next.data.api.model.search.SearchResult
+import me.ykrank.s1next.data.api.model.search.UserSearchWrapper
+import me.ykrank.s1next.databinding.ActivitySearchBinding
+import me.ykrank.s1next.databinding.AppBarSearchBinding
+import me.ykrank.s1next.view.adapter.SearchRecyclerViewAdapter
+import me.ykrank.s1next.view.transition.CircularReveal
+import me.ykrank.s1next.view.transition.TransitionCompatCreator
+import me.ykrank.s1next.widget.track.event.SearchTrackEvent
+import javax.inject.Inject
+
+/**
+ * Created by ykrank on 2016/9/28 0028.
+ */
+
+class SearchActivity : BaseActivity() {
+
+    @Inject
+    lateinit var mUserValidator: UserValidator
+    @Inject
+    lateinit var s1Service: S1Service
+
+    private lateinit var binding: ActivitySearchBinding
+
+    private lateinit var searchView: SearchView
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var searchBack: ImageButton
+    private lateinit var appBar: AppBarSearchBinding
+    private var noResults: TextView? = null
+
+    private lateinit var adapter: SearchRecyclerViewAdapter
+
+    private val autoTransitionCompat by lazy { TransitionCompatCreator.getAutoTransition() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        App.appComponent.inject(this)
+        super.onCreate(savedInstanceState)
+        L.leaveMsg("SearchActivity")
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_search)
+
+        appBar = binding.appBar!!
+        searchView = appBar.searchView
+        recyclerView = binding.searchResults
+        searchBack = appBar.searchback
+
+        appBar.toolbar.navigationIcon = null
+        setupWindowAnimations()
+        setupTransitions()
+        compatBackIcon()
+
+        adapter = SearchRecyclerViewAdapter(this)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        searchBack.setOnClickListener { dismiss() }
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        setupSearchView()
+    }
+
+    private fun setupWindowAnimations() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val enterTransition = TransitionInflater.from(this).inflateTransition(R.transition.search_enter)
+            window.enterTransition = enterTransition
+
+            val returnTransition = TransitionInflater.from(this).inflateTransition(R.transition.search_return)
+            window.returnTransition = returnTransition
+
+            val enterShareTransition = TransitionInflater.from(this).inflateTransition(R.transition.search_shared_enter)
+            window.sharedElementEnterTransition = enterShareTransition
+
+            val returnShareTransition = TransitionInflater.from(this).inflateTransition(R.transition.search_shared_return)
+            window.sharedElementReturnTransition = returnShareTransition
+        }
+    }
+
+    private fun setupTransitions() {
+        // grab the position that the search icon transitions in *from*
+        // & use it to configure the return transition
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setEnterSharedElementCallback(object : SharedElementCallback() {
+                @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+                override fun onSharedElementStart(
+                        sharedElementNames: List<String>,
+                        sharedElements: List<View>?,
+                        sharedElementSnapshots: List<View>) {
+                    if (sharedElements != null && !sharedElements.isEmpty()) {
+                        val searchIcon = sharedElements[0]
+                        if (searchIcon.id != R.id.searchback) return
+                        val centerX = (searchIcon.left + searchIcon.right) / 2
+                        val hideResults = TransitionUtils.findTransition(
+                                window.returnTransition as TransitionSet,
+                                CircularReveal::class.java, R.id.results_container) as CircularReveal?
+                        hideResults?.setCenter(Point(centerX, 0))
+                    }
+                }
+            })
+            // focus the search view once the transition finishes
+            window.enterTransition.addListener(
+                    object : TransitionUtils.TransitionListenerAdapter() {
+                        override fun onTransitionEnd(transition: Transition) {
+                            searchView.requestFocus()
+                            ImeUtils.showIme(searchView)
+                        }
+                    })
+        } else {
+            searchView.requestFocus()
+            ImeUtils.showIme(searchView)
+        }
+    }
+
+    private fun compatBackIcon() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            val drawable = StateListDrawable()
+
+            val colorPrimaryDark = ResourceUtil.getAttrColorInt(this, R.attr.colorPrimaryDark)
+            val colorPrimary = ResourceUtil.getAttrColorInt(this, R.attr.colorPrimary)
+
+            drawable.addState(intArrayOf(android.R.attr.state_pressed), ColorDrawable(colorPrimaryDark))
+            drawable.addState(intArrayOf(-android.R.attr.state_pressed), ColorDrawable(colorPrimary))
+
+
+            ViewCompat.setBackground(appBar.searchback, drawable)
+        }
+    }
+
+    private fun setupSearchView() {
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        // hint, inputType & ime options seem to be ignored from XML! Set in code
+        searchView.queryHint = getString(R.string.search_hint)
+        searchView.inputType = InputType.TYPE_TEXT_FLAG_CAP_WORDS
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            searchView.imeOptions = searchView.imeOptions or EditorInfo.IME_ACTION_SEARCH or
+                    EditorInfo.IME_FLAG_NO_EXTRACT_UI or EditorInfo.IME_FLAG_NO_FULLSCREEN
+        } else {
+            searchView.imeOptions = EditorInfo.IME_ACTION_SEARCH or
+                    EditorInfo.IME_FLAG_NO_EXTRACT_UI or EditorInfo.IME_FLAG_NO_FULLSCREEN
+        }
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                searchFor(query)
+                return true
+            }
+
+            override fun onQueryTextChange(query: String): Boolean {
+                if (TextUtils.isEmpty(query)) {
+                    clearResults(true)
+                }
+                return true
+            }
+        })
+    }
+
+    private fun clearResults(transition: Boolean) {
+        if (transition) {
+            TransitionManager.beginDelayedTransition(binding.coordinatorLayout, autoTransitionCompat)
+        }
+
+        recyclerView.visibility = View.GONE
+        binding.progressBar.visibility = View.GONE
+        binding.resultsScrim.visibility = View.GONE
+        setNoResultsVisibility(View.GONE, null)
+    }
+
+    private fun setResults(data: List<SearchResult>?, errorMsg: String?) {
+        if (data != null && data.isNotEmpty()) {
+            if (recyclerView.visibility != View.VISIBLE) {
+                TransitionManager.beginDelayedTransition(binding.resultsContainer, autoTransitionCompat)
+                binding.progressBar.visibility = View.GONE
+                recyclerView.visibility = View.VISIBLE
+            }
+            adapter.swapDataSet(data)
+        } else {
+            TransitionManager.beginDelayedTransition(binding.resultsContainer, autoTransitionCompat)
+            binding.progressBar.visibility = View.GONE
+            setNoResultsVisibility(View.VISIBLE, errorMsg)
+        }
+    }
+
+    private fun setNoResultsVisibility(visibility: Int, errorMsg: String?) {
+        if (visibility == View.VISIBLE) {
+            if (noResults == null) {
+                noResults = binding.stubNoSearchResults.viewStub?.inflate() as TextView?
+                noResults?.setOnClickListener {
+                    searchView.setQuery("", false)
+                    searchView.requestFocus()
+                    ImeUtils.showIme(searchView)
+                }
+            }
+            if (TextUtils.isEmpty(errorMsg)) {
+                val message = String.format(
+                        getString(R.string.no_search_results), searchView.query.toString())
+                val ssb = SpannableStringBuilder(message)
+                ssb.setSpan(StyleSpan(Typeface.ITALIC),
+                        message.indexOf('“') + 1,
+                        message.length - 1,
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                noResults?.text = ssb
+            } else {
+                noResults?.text = errorMsg
+            }
+        }
+        if (noResults != null) {
+            noResults?.visibility = visibility
+        }
+    }
+
+    private fun searchFor(query: String) {
+        trackAgent.post(SearchTrackEvent(query))
+
+        clearResults(false)
+        binding.progressBar.visibility = View.VISIBLE
+        ImeUtils.hideIme(searchView)
+        searchView.clearFocus()
+        //        dataManager.searchFor(query);
+
+        val selected = appBar.spinner.selectedItem as String
+        if (TextUtils.equals(getString(R.string.search_type_entry_user), selected)) {
+            ApiFlatTransformer.flatMappedWithAuthenticityToken(s1Service, mUserValidator, mUser) { token -> s1Service.searchUser(token, query) }
+                    .map { UserSearchWrapper.fromSource(it) }
+                    .compose(RxJavaUtil.iOSingleTransformer())
+                    .to(AndroidRxDispose.withSingle(this, ActivityEvent.DESTROY))
+                    .subscribe({ wrapper -> setResults(wrapper.userSearchResults, wrapper.errorMsg) }, L::e)
+        } else {
+            ApiFlatTransformer.flatMappedWithAuthenticityToken(s1Service, mUserValidator, mUser) { token -> s1Service.searchForum(token, query) }
+                    .map { ForumSearchWrapper.fromSource(it) }
+                    .compose(RxJavaUtil.iOSingleTransformer())
+                    .to(AndroidRxDispose.withSingle(this, ActivityEvent.DESTROY))
+                    .subscribe({ wrapper -> setResults(wrapper.forumSearchResults, null) }, L::e)
+        }
+    }
+
+    private fun dismiss() {
+        // clear the background else the touch ripple moves with the translation which looks bad
+        ViewCompat.setBackground(searchBack, null)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            finishAfterTransition()
+        } else {
+            finish()
+        }
+    }
+
+    companion object {
+        val TAG = BaseActivity::class.java.name
+
+        fun start(context: Context) {
+            context.startActivity(Intent(context, SearchActivity::class.java))
+        }
+
+        fun start(activity: Activity, searchIconView: View) {
+            val options = ActivityOptionsCompat.makeSceneTransitionAnimation(activity, searchIconView,
+                    activity.getString(R.string.transition_search_back)).toBundle()
+            ActivityCompat.startActivity(activity, Intent(activity, SearchActivity::class.java), options)
+        }
+    }
+}
