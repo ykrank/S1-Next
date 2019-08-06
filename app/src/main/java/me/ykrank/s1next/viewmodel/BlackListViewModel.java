b9283455531e419f87fcf863a45846d731e97ac2
ykrank <ykrank@gmail.com>
2019-08-06 17:24:39 +0800

    Change snackbar max line

--- app/src/main/java/me/ykrank/s1next/viewmodel/BlackListViewModel.java
+++ app/src/main/java/me/ykrank/s1next/viewmodel/BlackListViewModel.java
@@ -1,11 +1,15 @@
 package me.ykrank.s1next.viewmodel;
 
-import androidx.databinding.ObservableField;
-import com.google.android.material.snackbar.Snackbar;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.databinding.ObservableField;
+
+import com.google.android.material.snackbar.Snackbar;
+
 import me.ykrank.s1next.data.db.dbmodel.BlackList;
+
+import static com.github.ykrank.androidtools.ui.internal.CoordinatorLayoutAnchorDelegateBaseImpl.SNACK_BAR_MAX_LINE;
 
 
 public final class BlackListViewModel {
@@ -13,8 +17,11 @@ public final class BlackListViewModel {
     public final ObservableField<BlackList> blacklist = new ObservableField<>();
 
     public View.OnClickListener clickSnackbar() {
-        return v -> Snackbar.make(v.getRootView(), ((TextView) v).getText(),
-                Snackbar.LENGTH_SHORT)
-                .show();
+        return v -> {
+            Snackbar snackbar = Snackbar.make(v.getRootView(), ((TextView) v).getText(), Snackbar.LENGTH_SHORT);
+            TextView textView = snackbar.getView().findViewById(com.google.android.material.R.id.snackbar_text);
+            textView.setMaxLines(SNACK_BAR_MAX_LINE);
+            snackbar.show();
+        };
     }
 }
