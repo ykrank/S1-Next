c2be3f8b7ef9064860f3390ffb783171547e285c
ykrank <ykrank@gmail.com>
2019-11-15 12:27:17 +0800

    Image file name use base64 url, not md5

--- app/src/main/java/me/ykrank/s1next/data/api/ApiUtil.java
+++ app/src/main/java/me/ykrank/s1next/data/api/ApiUtil.java
@@ -1,5 +1,7 @@
 package me.ykrank.s1next.data.api;
 
+import android.util.Base64;
+
 import androidx.annotation.NonNull;
 
 /**
@@ -11,5 +13,9 @@ public class ApiUtil {
     @NonNull
     public static String replaceAjaxHeader(@NonNull String html) {
         return html.replace("<root><![CDATA[", "").replace("]]></root>", "");
+    }
+
+    public static String getUrlId(String url) {
+        return Base64.encodeToString(url.getBytes(), Base64.URL_SAFE);
     }
 }
--- app/src/main/java/me/ykrank/s1next/widget/download/ImageDownloadManager.kt
+++ app/src/main/java/me/ykrank/s1next/widget/download/ImageDownloadManager.kt
@@ -5,18 +5,15 @@ import com.liulishuo.okdownload.DownloadTask
 import com.liulishuo.okdownload.OkDownload
 import com.liulishuo.okdownload.core.connection.DownloadOkHttp3Connection
 import com.liulishuo.okdownload.core.dispatcher.DownloadDispatcher
-import me.ykrank.s1next.util.md5
+import me.ykrank.s1next.data.api.ApiUtil
 import okhttp3.OkHttpClient
 import java.io.File
-import java.security.MessageDigest
 
 /**
  * Created by ykrank on 2017/11/14.
  */
 class ImageDownloadManager(private val okhttpBuilder: OkHttpClient.Builder) {
     private lateinit var dir: File
-
-    private val digest = MessageDigest.getInstance("MD5")
 
     fun setup(application: Application) {
         val builder = OkDownload.Builder(application)
@@ -36,7 +33,7 @@ class ImageDownloadManager(private val okhttpBuilder: OkHttpClient.Builder) {
                 // ignore the same task has already completed in the past.
                 .setPassIfAlreadyCompleted(true)
                 .setFilenameFromResponse(false)
-                .setFilename(digest.md5(url))
+                .setFilename(ApiUtil.getUrlId(url))
                 .build()
         val imageDownloadTask = ImageDownloadTask(task)
         imageDownloadTask.enqueue(downloadListener)
