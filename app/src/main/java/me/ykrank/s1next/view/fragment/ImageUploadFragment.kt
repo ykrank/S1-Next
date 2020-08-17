6c0bff01c3e51919cd0ecc95aaa6faf2358be495
ykrank <ykrank@gmail.com>
2020-08-17 18:53:18 +0800

    RIP上传接口

--- app/src/main/java/me/ykrank/s1next/view/fragment/ImageUploadFragment.kt
+++ app/src/main/java/me/ykrank/s1next/view/fragment/ImageUploadFragment.kt
@@ -4,17 +4,25 @@ import android.os.Bundle
 import android.view.View
 import com.github.ykrank.androidtools.widget.RxBus
 import com.github.ykrank.androidtools.widget.imagepicker.ImagePicker
+import com.github.ykrank.androidtools.widget.uploadimg.ImageUploadManager
 import com.github.ykrank.androidtools.widget.uploadimg.LibImageUploadFragment
 import com.github.ykrank.androidtools.widget.uploadimg.ModelImageUpload
 import com.luck.picture.lib.config.PictureConfig
 import me.ykrank.s1next.App
 import me.ykrank.s1next.view.event.PostAddImageEvent
+import me.ykrank.s1next.widget.net.Image
+import me.ykrank.s1next.widget.uploadimg.RIPImageUploadManager
+import okhttp3.OkHttpClient
 import javax.inject.Inject
 
 class ImageUploadFragment : LibImageUploadFragment() {
 
     @Inject
     internal lateinit var mRxBus: RxBus
+
+    @Inject
+    @Image
+    internal lateinit var mOkHttpClient: OkHttpClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +36,10 @@ class ImageUploadFragment : LibImageUploadFragment() {
         ImagePicker.pickImage(this, PictureConfig.CHOOSE_REQUEST, 5, false)
     }
 
+    override fun provideImageUploadManager(): ImageUploadManager {
+        return RIPImageUploadManager(_okHttpClient = mOkHttpClient)
+    }
+
     companion object {
         val TAG: String = ImageUploadFragment::class.java.name
 
@@ -36,3 +48,4 @@ class ImageUploadFragment : LibImageUploadFragment() {
         }
     }
 }
+
