236ab832667ea62b5627a29a1b55249ff3a61017
ykrank <ykrank@gmail.com>
2024-06-12 14:35:55 +0800

    处理链接跳转

--- app/src/main/java/me/ykrank/s1next/widget/span/BilibiliSpan.kt
+++ app/src/main/java/me/ykrank/s1next/widget/span/BilibiliSpan.kt
@@ -1,162 +1,131 @@
-package me.ykrank.s1next.widget.span;
+package me.ykrank.s1next.widget.span
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import androidx.annotation.NonNull;
-import androidx.core.util.Pair;
-import android.text.Spannable;
-import android.text.SpannableStringBuilder;
-import android.text.style.ClickableSpan;
-import android.view.View;
-
-import com.github.ykrank.androidtools.util.L;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ClickableSpan
+import android.view.View
+import com.github.ykrank.androidtools.util.L
+import me.ykrank.s1next.widget.span.PostMovementMethod.URLSpanClick
 
 /**
  * <xmp>
  * 符合intent-filter的<a></a> tag。或者<bilibili></bilibili> tag
- * </xmp>
- * <br>
+</xmp> *
+ * <br></br>
  * <xmp>
- * eg:<a href="http://www.bilibili.com/video/av6591319/" >http://www.bilibili.com/video/av6591319/</a>
- * </xmp>
- * <br>
+ * eg:[http://www.bilibili.com/video/av6591319/](http://www.bilibili.com/video/av6591319/)
+</xmp> *
+ * <br></br>
  * <xmp>
  * or <bilibili>http://www.bilibili.com/video/av6591319/</bilibili>
- * </xmp>
- * <br>
+</xmp> *
+ * <br></br>
  * Created by ykrank on 2016/10/16 0016.
  */
-
-public class BilibiliSpan implements PostMovementMethod.URLSpanClick {
-    /**
-     * intent-filter.从官方APP AndroidManifest中提取
-     */
-    private static List<Pair<String, String>> HOST_FILTERS = new ArrayList<>();
-
-    static {
-        HOST_FILTERS.add(new Pair<>("space.bilibili.com", ".*"));
-        HOST_FILTERS.add(new Pair<>("bilibili.kankanews.com", "/video/av.*"));
-        HOST_FILTERS.add(new Pair<>("bilibili.tv", "/video/av.*"));
-        HOST_FILTERS.add(new Pair<>("bilibili.cn", "/video/av.*"));
-        HOST_FILTERS.add(new Pair<>("bilibili.com", "/video/av.*"));
-        HOST_FILTERS.add(new Pair<>("www.bilibili.tv", "/video/av.*"));
-        HOST_FILTERS.add(new Pair<>("www.bilibili.cn", "/video/av.*"));
-        HOST_FILTERS.add(new Pair<>("www.bilibili.com", "/video/av.*"));
-        HOST_FILTERS.add(new Pair<>("bilibili.smgbb.cn", "/video/av.*"));
-        HOST_FILTERS.add(new Pair<>("m.acg.tv", "/video/av.*"));
-        HOST_FILTERS.add(new Pair<>("www.bilibili.com", "/mobile/video/av.*"));
-        HOST_FILTERS.add(new Pair<>("live.bilibili.com", "/live/*.html"));
-        HOST_FILTERS.add(new Pair<>("www.bilibili.com", "/bangumi/i/.*"));
-        HOST_FILTERS.add(new Pair<>("www.bilibili.com", "/mobile/bangumi/i/.*"));
-        HOST_FILTERS.add(new Pair<>("bangumi.bilibili.com", "/anime/.*"));
-        HOST_FILTERS.add(new Pair<>("bangumi.bilibili.com", "/anime/category/.*"));
-        HOST_FILTERS.add(new Pair<>("bilibili.com", "/sp/.*"));
-        HOST_FILTERS.add(new Pair<>("www.bilibili.com", "/sp/.*"));
-        HOST_FILTERS.add(new Pair<>("bilibili.tv", "/sp/.*"));
-        HOST_FILTERS.add(new Pair<>("www.bilibili.tv", "/sp/.*"));
-    }
-
-    /**
-     * See android.text.HtmlToSpannedConverter#startA(android.text.SpannableStringBuilder, org.xml.sax.Attributes)
-     */
-    public static void startBilibiliSpan(SpannableStringBuilder text) {
-        int len = text.length();
-        text.setSpan(new BilibiliHref(), len, len, Spannable.SPAN_MARK_MARK);
-    }
-
-    /**
-     * See android.text.HtmlToSpannedConverter#endA(android.text.SpannableStringBuilder)
-     */
-    public static void endBilibiliSpan(@NonNull SpannableStringBuilder text) {
-        int len = text.length();
-        Object obj = TagHandler.getLastSpan(text, BilibiliHref.class);
-        int where = text.getSpanStart(obj);
-
-        text.removeSpan(obj);
-
-        if (where < len && obj != null) {
-            char[] hrefChars = new char[len - where];
-            text.getChars(where, len, hrefChars, 0);
-            String href = String.valueOf(hrefChars);
-
-            text.setSpan(new BilibiliURLSpan(href), where, len,
-                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        }
-    }
-
-    // 对Bilibili链接进行独立处理，调用Bilibili客户端
-    private static void goBilibili(Context context, Uri uri) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        intent.setClassName("tv.danmaku.bili", "tv.danmaku.bili.ui.IntentHandlerActivity");
-        try {
-            PackageManager pm = context.getPackageManager();
-            ActivityInfo ai = intent.resolveActivityInfo(pm, PackageManager.MATCH_DEFAULT_ONLY);
-            if (ai == null) {
-                context.startActivity(new Intent(Intent.ACTION_VIEW, uri));
-            } else {
-                context.startActivity(intent);
-            }
-        } catch (Throwable e) {
-            L.report("BilibiliURLSpan startActivity error for intent, " + intent.toString(), e);
-            context.startActivity(new Intent(Intent.ACTION_VIEW, uri));
-        }
-    }
-
-    @Override
-    public boolean isMatch(Uri uri) {
-        if ("http".equalsIgnoreCase(uri.getScheme())) {
-            String path = uri.getEncodedPath();
+class BilibiliSpan : URLSpanClick {
+    override fun isMatch(uri: Uri): Boolean {
+        if ("http".equals(uri.scheme, ignoreCase = true) ||
+            "https".equals(uri.scheme, ignoreCase = true)
+        ) {
+            var path = uri.encodedPath
             if (path == null) {
-                path = "/";
+                path = "/"
             }
-            for (Pair<String, String> filter : HOST_FILTERS) {
-                String host = filter.first;
-                String pathPattern = filter.second;
-
-                if (host.equalsIgnoreCase(uri.getHost())
-                        && path.matches(pathPattern)) {
-                    return true;
+            for (filter in HOST_FILTERS) {
+                val hostFilter = filter.key
+                val pathPattern = filter.value
+                var host = uri.host ?: ""
+                if (host.startsWith("www.")) {
+                    host = host.replaceFirst("www.", "")
+                }
+                if (hostFilter.equals(host, ignoreCase = true)
+                    && path.matches(pathPattern.toRegex())
+                ) {
+                    return true
                 }
             }
         }
-        return false;
+        return false
     }
 
-    @Override
-    public void onClick(Uri uri, View view) {
-        goBilibili(view.getContext(), uri);
+    override fun onClick(uri: Uri, view: View) {
+        goBilibili(view.context, uri)
     }
 
-    private static class BilibiliHref {
+    private class BilibiliHref
+    private class BilibiliURLSpan(val uRL: String) : ClickableSpan() {
 
-    }
-
-    private static class BilibiliURLSpan extends ClickableSpan {
-
-        private final String mURL;
-
-        BilibiliURLSpan(String url) {
-            mURL = url;
-        }
-
-        String getURL() {
-            return mURL;
-        }
-
-        @Override
-        public void onClick(View widget) {
-            goBilibili(widget.getContext(), Uri.parse(getURL()));
+        override fun onClick(widget: View) {
+            goBilibili(widget.context, Uri.parse(uRL))
         }
     }
 
-    private static class HostFilter {
-        private String host;
-        private String filter;
+    companion object {
+        /**
+         * intent-filter.从官方APP AndroidManifest中提取
+         */
+        private val HOST_FILTERS: Map<String, String> by lazy {
+            buildMap {
+                put("bilibili.com", ".*")
+                put("bilibili.tv", ".*")
+                put("bilibili.cn", ".*")
+                put("bangumi.bilibili.com", ".*")
+
+                put("bilibili.kankanews.com", "/video/av.*")
+                put("bilibili.smgbb.cn", "/video/av.*")
+                put("m.acg.tv", "/video/av.*")
+            }
+        }
+
+        /**
+         * See android.text.HtmlToSpannedConverter#startA(android.text.SpannableStringBuilder, org.xml.sax.Attributes)
+         */
+        @JvmStatic
+        fun startBilibiliSpan(text: SpannableStringBuilder) {
+            val len = text.length
+            text.setSpan(BilibiliHref(), len, len, Spannable.SPAN_MARK_MARK)
+        }
+
+        /**
+         * See android.text.HtmlToSpannedConverter#endA(android.text.SpannableStringBuilder)
+         */
+        @JvmStatic
+        fun endBilibiliSpan(text: SpannableStringBuilder) {
+            val len = text.length
+            val obj: Any? = TagHandler.getLastSpan(text, BilibiliHref::class.java)
+            val where = text.getSpanStart(obj)
+            text.removeSpan(obj)
+            if (where < len && obj != null) {
+                val hrefChars = CharArray(len - where)
+                text.getChars(where, len, hrefChars, 0)
+                val href = String(hrefChars)
+                text.setSpan(
+                    BilibiliURLSpan(href), where, len,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+            }
+        }
+
+        // 对Bilibili链接进行独立处理，调用Bilibili客户端
+        private fun goBilibili(context: Context, uri: Uri) {
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            intent.setClassName("tv.danmaku.bili", "tv.danmaku.bili.ui.IntentHandlerActivity")
+            try {
+                val pm = context.packageManager
+                val ai = intent.resolveActivityInfo(pm, PackageManager.MATCH_DEFAULT_ONLY)
+                if (ai == null) {
+                    context.startActivity(Intent(Intent.ACTION_VIEW, uri))
+                } else {
+                    context.startActivity(intent)
+                }
+            } catch (e: Throwable) {
+                L.report("BilibiliURLSpan startActivity error for intent, $intent", e)
+                context.startActivity(Intent(Intent.ACTION_VIEW, uri))
+            }
+        }
     }
 }
--- app/src/main/java/me/ykrank/s1next/widget/span/SarabaSpan.kt
+++ app/src/main/java/me/ykrank/s1next/widget/span/SarabaSpan.kt
@@ -1,84 +1,84 @@
-package me.ykrank.s1next.widget.span;
+package me.ykrank.s1next.widget.span
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.view.View;
-
-import androidx.core.util.Pair;
-
-import com.github.ykrank.androidtools.util.L;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import me.ykrank.s1next.App;
-import me.ykrank.s1next.view.page.post.postlist.PostListGatewayActivity;
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.view.View
+import com.github.ykrank.androidtools.util.L
+import me.ykrank.s1next.view.page.post.postlist.PostListGatewayActivity
+import me.ykrank.s1next.widget.span.PostMovementMethod.URLSpanClick
 
 /**
  * <xmp>
  * 符合intent-filter的<a></a> tag。
- * </xmp>
- * <br>
+</xmp> *
+ * <br></br>
  * <xmp>
- * eg:<a href="http://bbs.saraba1st.com/2b/forum-75-1.html" >http://bbs.saraba1st.com/2b/forum-75-1.html</a>
- * </xmp>
- * <br>
+ * eg:[http://bbs.saraba1st.com/2b/forum-75-1.html](http://bbs.saraba1st.com/2b/forum-75-1.html)
+</xmp> *
+ * <br></br>
  * Created by ykrank on 2016/10/16 0016.
  */
-
-public class SarabaSpan implements PostMovementMethod.URLSpanClick {
-    /**
-     * intent-filter.从AndroidManifest中提取
-     */
-    private static List<Pair<String, String>> HOST_FILTERS = new ArrayList<>();
-
-    static {
-        HOST_FILTERS.add(new Pair<>("bbs.saraba1st.com", ".*"));
-    }
-
-    // 对Saraba链接进行独立处理，调用Saraba客户端
-    protected static void goSaraba(Context context, Uri uri) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        intent.setClass(context, PostListGatewayActivity.class);
-        try {
-            PackageManager pm = context.getPackageManager();
-            ActivityInfo ai = intent.resolveActivityInfo(pm, PackageManager.MATCH_DEFAULT_ONLY);
-            if (ai == null) {
-                context.startActivity(new Intent(Intent.ACTION_VIEW, uri));
-            } else {
-                context.startActivity(intent);
+open class SarabaSpan : URLSpanClick {
+    override fun isMatch(uri: Uri): Boolean {
+        if ("http".equals(uri.scheme, ignoreCase = true) ||
+            "https".equals(uri.scheme, ignoreCase = true)
+        ) {
+            var path = uri.encodedPath
+            if (path == null) {
+                path = "/"
             }
-        } catch (Throwable e) {
-            L.report("SarabaURLSpan startActivity error for intent, " + intent.toString(), e);
-            context.startActivity(new Intent(Intent.ACTION_VIEW, uri));
+            for (filter in HOST_FILTERS) {
+                val hostFilter = filter.key
+                val pathPattern = filter.value
+                var host = uri.host ?: ""
+                if (host.startsWith("www.")) {
+                    host = host.replaceFirst("www.", "")
+                }
+                if (hostFilter.equals(host, ignoreCase = true)
+                    && path.matches(pathPattern.toRegex())
+                ) {
+                    return true
+                }
+            }
         }
+        return false
     }
 
-    private static boolean matchSarabaInfo(Context context, Uri uri){
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        try {
-            PackageManager pm = context.getPackageManager();
-            ActivityInfo ai = intent.resolveActivityInfo(pm, PackageManager.MATCH_DEFAULT_ONLY);
-            return ai != null;
-        } catch (Throwable e) {
-            L.d("url is not match s1");
-        }
-        return false;
+    override fun onClick(uri: Uri, view: View) {
+        goSaraba(view.context, uri)
     }
 
-    @Override
-    public boolean isMatch(Uri uri) {
-        if ("http".equalsIgnoreCase(uri.getScheme()) || "https".equalsIgnoreCase(uri.getScheme())) {
-            return matchSarabaInfo(App.Companion.get(), uri);
+    companion object {
+        /**
+         * intent-filter.从AndroidManifest中提取
+         */
+        private val HOST_FILTERS: Map<String, String> by lazy {
+            buildMap {
+                put("saraba1st.com", ".*")
+                put("stage1.cc", ".*")
+                put("stage1st.com", ".*")
+            }
         }
-        return false;
-    }
 
-    @Override
-    public void onClick(Uri uri, View view) {
-        goSaraba(view.getContext(), uri);
+        // 对Saraba链接进行独立处理，调用Saraba客户端
+        @JvmStatic
+        protected fun goSaraba(context: Context, uri: Uri?) {
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            intent.setClass(context, PostListGatewayActivity::class.java)
+            try {
+                val pm = context.packageManager
+                val ai = intent.resolveActivityInfo(pm, PackageManager.MATCH_DEFAULT_ONLY)
+                if (ai == null) {
+                    context.startActivity(Intent(Intent.ACTION_VIEW, uri))
+                } else {
+                    context.startActivity(intent)
+                }
+            } catch (e: Throwable) {
+                L.report("SarabaURLSpan startActivity error for intent, $intent", e)
+                context.startActivity(Intent(Intent.ACTION_VIEW, uri))
+            }
+        }
     }
 }
