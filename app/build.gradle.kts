3122169f4c0c37f215fd082f1212783f35642d47
ykrank <ykrank@gmail.com>
2024-08-24 15:50:14 +0800

    release 3.1.90

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ val mKeyPassword: String? = properties.getProperty("keyPassword")
 val httpDnsId = properties.getProperty("httpDnsId") ?: "\"\""
 val httpDnsSecret = properties.getProperty("httpDnsSecret") ?: "\"\""
 
-val appVersionCode = 89
+val appVersionCode = 90
 val appVersionName = "3.1"
 val appVersionSuffix = ""
 
