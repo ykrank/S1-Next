543a9caef7fcef2025d6a8f11b419c6dfbbcdde7
ykrank <ykrank@gmail.com>
2024-11-05 22:00:42 +0800

    提升评分接口的并发速度

--- app/src/main/java/me/ykrank/s1next/data/cache/api/S1ApiCacheProvider.kt
+++ app/src/main/java/me/ykrank/s1next/data/cache/api/S1ApiCacheProvider.kt
@@ -9,13 +9,13 @@ import com.github.ykrank.androidtools.util.L
 import com.github.ykrank.androidtools.widget.LoadTime
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.combineTransform
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import me.ykrank.s1next.BuildConfig
@@ -23,6 +23,7 @@ import me.ykrank.s1next.data.User
 import me.ykrank.s1next.data.api.ApiCacheProvider
 import me.ykrank.s1next.data.api.ApiUtil
 import me.ykrank.s1next.data.api.S1Service
+import me.ykrank.s1next.data.api.model.Post
 import me.ykrank.s1next.data.api.model.Rate
 import me.ykrank.s1next.data.api.model.wrapper.ForumGroupsWrapper
 import me.ykrank.s1next.data.api.model.wrapper.PostsWrapper
@@ -204,7 +205,6 @@ class S1ApiCacheProvider(
             }
         }
 
-        var netPostsWrapper: PostsWrapper? = null
         // 评分缓存过期的回帖，先展示缓存，然后再刷新
         val outdatedRatePostIds = mutableListOf<Int>()
         return apiCacheFlow.getFlow()
@@ -219,15 +219,14 @@ class S1ApiCacheProvider(
                             }
                         }
 
-                        //Set comment init info(if it has comment)
+                        //初始化评分数量信息
                         ratePostWrapper.data?.data?.commentCountMap?.apply {
                             postWrapper?.data?.initCommentCount(this)
                         }
 
-                        val postList = postWrapper?.data?.postList
-                        if (!postList.isNullOrEmpty()) {
+                        // 交易帖
+                        suspend fun doTrade(postList: List<Post>) {
                             val post = postList[0]
-                            // 交易帖
                             if (post.isTrade) {
                                 post.extraHtml = ""
                                 runCatching {
@@ -242,19 +241,28 @@ class S1ApiCacheProvider(
                                     }
                                 }
                             }
+                        }
 
-                            //从内存缓存中获取评分详情。
-                            postList.filter { it.rates?.size == 0 }.forEach {
-                                val cacheKey = getRateKey(threadId, it.id)
-                                val cache = ratesCache[cacheKey]
+                        //从内存缓存中获取评分详情。
+                        fun loadRatesFromCache(it: Post) {
+                            val cacheKey = getRateKey(threadId, it.id)
+                            val cache = ratesCache[cacheKey]
 
-                                if (cache != null) {
-                                    val rates = cache.data
-                                    it.rates = rates
-                                    if (System.currentTimeMillis() - cache.time > CACHE_RATE_MILLS) {
-                                        outdatedRatePostIds.add(it.id)
-                                    }
+                            if (cache != null) {
+                                val rates = cache.data
+                                it.rates = rates
+                                if (System.currentTimeMillis() - cache.time > CACHE_RATE_MILLS) {
+                                    outdatedRatePostIds.add(it.id)
                                 }
+                            }
+                        }
+
+                        val postList = postWrapper?.data?.postList
+                        if (!postList.isNullOrEmpty()) {
+                            doTrade(postList)
+
+                            postList.filter { it.rates?.size == 0 }.forEach {
+                                loadRatesFromCache(it)
                             }
                         }
                         if (!hasError && postWrapper != null && cacheValid) {
@@ -265,60 +273,66 @@ class S1ApiCacheProvider(
                             }
                         }
                     }
+
+                    val postsWrapper = it.data
+                    // 获取评分详情。
+                    if (onRateUpdate != null && postsWrapper != null) {
+                        coroutineScope {
+                            launch {
+                                val posts = postsWrapper.data?.postList
+                                if (posts != null) {
+                                    withContext(Dispatchers.IO) {
+                                        posts.filter {
+                                            it.rates?.size == 0 || outdatedRatePostIds.contains(
+                                                it.id
+                                            )
+                                        }
+                                            .distinctBy { it.id }
+                                            .asFlow()
+                                            .map {
+                                                val pid = it.id
+                                                val cacheKey = getRateKey(threadId, it.id)
+                                                val rateHtml = loadTime.run("get_rate_${it.id}") {
+                                                    s1Service.getRates(threadId, pid.toString())
+                                                }
+                                                val rates = Rate.fromHtml(rateHtml).apply {
+                                                    ratesCache.put(
+                                                        cacheKey,
+                                                        BaseCache(System.currentTimeMillis(), this)
+                                                    )
+                                                }
+                                                // 最新数据和缓存不同时，才刷新
+                                                if (it.rates != rates) {
+                                                    it.rates = rates
+                                                    launch(Dispatchers.Main) {
+                                                        onRateUpdate(pid, rates)
+                                                    }
+                                                    pid
+                                                } else {
+                                                    null
+                                                }
+                                            }.debounce(CACHE_RATE_SAVE_DEBOUNCE)
+                                            .collect {
+                                                if (cacheValid && it != null) {
+                                                    withContext(Dispatchers.Default) {
+                                                        loadTime.run(ApiCacheConstants.Time.TIME_UPDATE_CACHE + it) {
+                                                            saveCache(postsWrapper)
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
                     emit(it)
                 } else if (it.source.isCache() && ratePostWrapper.source.isCache()) {
                     emit(it)
                 }
-            }.onEach {
-                if (it.source.isCloud()) {
-                    netPostsWrapper = it.data
-                }
             }.onCompletion {
                 loadTime.addPoint("completion")
-                // 获取评分详情。
-                val postsWrapper = netPostsWrapper
-                if (onRateUpdate != null && postsWrapper != null) {
-                    val posts = postsWrapper.data?.postList
-                    if (posts != null) {
-                        withContext(Dispatchers.IO) {
-                            posts.filter { it.rates?.size == 0 || outdatedRatePostIds.contains(it.id) }
-                                .distinctBy { it.id }
-                                .asFlow()
-                                .map {
-                                    val pid = it.id
-                                    val cacheKey = getRateKey(threadId, it.id)
-                                    val rateHtml = loadTime.run("get_rate_${it.id}") {
-                                                s1Service.getRates(threadId, pid.toString())
-                                            }
-                                    val rates = Rate.fromHtml(rateHtml).apply {
-                                                ratesCache.put(
-                                                    cacheKey,
-                                                    BaseCache(System.currentTimeMillis(), this)
-                                                )
-                                            }
-                                    // 最新数据和缓存不同时，才刷新
-                                    if (it.rates != rates) {
-                                        it.rates = rates
-                                        launch(Dispatchers.Main) {
-                                            onRateUpdate(pid, rates)
-                                        }
-                                        pid
-                                    } else {
-                                        null
-                                    }
-                                }.debounce(CACHE_RATE_SAVE_DEBOUNCE)
-                                .collect {
-                                    if (cacheValid && it != null) {
-                                        withContext(Dispatchers.Default) {
-                                            loadTime.run(ApiCacheConstants.Time.TIME_UPDATE_CACHE + it) {
-                                                saveCache(postsWrapper)
-                                            }
-                                        }
-                                    }
-                                }
-                        }
-                    }
-                }
                 if (BuildConfig.DEBUG) {
                     loadTime.addPoint(ApiCacheConstants.Time.TIME_LOAD_END)
                     L.i(
@@ -333,7 +347,7 @@ class S1ApiCacheProvider(
         return "u${user.uid ?: ""}#${threadId ?: ""}#$pid"
     }
 
-    private suspend fun getRatePostsWrapper(
+    private fun getRatePostsWrapper(
         threadId: String,
         page: Int,
         authorId: String?,
